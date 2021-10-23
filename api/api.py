b462682659a825ad13af8c89c946e628015e1bf1
Joe Zakielarz <joseph.zakielarz@duke.edu>
2021-10-23 16:28:40 -0400

    Venv

---
+++ api/api.py
@@ -0,0 +1,4 @@
+# input:
+{"id1": {""" All of the attributes """}, "id2": {
+    """ All of the attributes """}, "id3": {""" All of the attributes """}}
+{"name": "Math", "mandatory": "Y", "start": 93, "stop": 103}
